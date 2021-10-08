f59607fba09ef0b0ee0e78b8f6229a1b28a7806a
anndonnelly <78274179+anndonnelly@users.noreply.github.com>
2021-10-08 00:35:19 -0400

    finished

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import { ModalProvider } from "./context/Modal";
+
 import configureStore from "./store";
 import { restoreCSRF, csrfFetch } from "./store/csrf";
 import * as sessionActions from "./store/session";
@@ -18,20 +20,17 @@ if (process.env.NODE_ENV !== "production") {
   window.sessionActions = sessionActions;
 }
 
-if (process.env.NODE_ENV !== "production") {
-  window.store = store;
-}
-
 function Root() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <ModalProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ModalProvider>
     </Provider>
   );
 }
-
 
 ReactDOM.render(
   <React.StrictMode>
